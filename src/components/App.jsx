5203d078bcf39cc0df5af3092f55bb992d3096a7
Gerhard Kordmann <gkordmann@sdl.com>
2026-04-13 16:51:03 +0100

    diferent handlers for login and worker clicks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,27 @@ export default function App() {
   const [loggedIn, setLoggedIn ] =  useState(false);
   console.log("Entering App()");
  
-  // this would be the place where in an old fashion program you could do some preparation.  
-  // not good for react, since it it comes here multiple times
- 
-  function handleClick() {
+  function handleClickLogin() {
   console.log("getLogState in callback()");
-    setLoggedIn(!loggedIn)
-  }
-  
+
+  //this is the place where the heavy lifting would happen: the actual login, 
+  // and the initialisation of data .
+
+    setLoggedIn(!loggedIn); 
+  } 
+
+  function handleClick() {
+    console.log("getLogState in callback()");
+    setLoggedIn(!loggedIn); 
+  } 
+
   return (
   <>
     <h3>React test app </h3>
       {loggedIn ? 
       <WorkerPage onClick={handleClick} />
       :
-      <LoginPage onClick={handleClick} />
+      <LoginPage onClick={handleClickLogin} />
       }
   </>
     )
